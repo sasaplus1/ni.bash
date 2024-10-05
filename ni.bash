de63003cdaa93bf7235affba7424525a9b34c72c
sasaplus1 <sasaplus1@gmail.com>
2024-10-05 23:52:36 +0900

    add help message

--- ni.bash
+++ ni.bash
@@ -166,14 +166,14 @@ __ni-option-help() {
 	Usage: ni [command] [options...] [args...]
 	
 	  a, aa                                alias of package manager
-	  add [-D | --save-dev] <packages...>
-	  ci
-	  dlx, npx [args...]
-	  i, install
-	  rm, remove <packages...>
-	  run <script> [args...]
-	  t, test [args...]
-	  up, upgrade
+	  add [-D | --save-dev] <packages...>  same as add
+	  ci                                   same as ci
+	  dlx, npx [args...]                   same as dlx
+	  i, install                           same as install
+	  rm, remove <packages...>             same as remove
+	  run <script> [args...]               same as run
+	  t, test [args...]                    same as test
+	  up, upgrade                          same as upgrade
 	  which                                show package manager name
 	
 	Usage: ni [option]
